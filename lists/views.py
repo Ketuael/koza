71bd700082d9ca0fdd77fe702ab1cf3d32d8d39f
Ketuael <ketuael@gmail.com>
2020-03-30 19:28:13 +0200

    Uzyto Item_Form do głównego widoku; zmieniono ID oraz name dla wejscia; still broken.

--- lists/views.py
+++ lists/views.py
@@ -1,17 +1,18 @@
 from django.shortcuts import render, redirect
 from django.core.exceptions import ValidationError
 from .models import Item, List
+from .forms import ItemForm
 
 # Create your views here.
 
 
 def home_page(request):
-    return render(request, 'lists/home.html', {})
+    return render(request, 'lists/home.html', {'form': ItemForm()})
 
 
 def new_list(request):
     list_ = List.objects.create()
-    item = Item(text=request.POST['item_text'], list=list_)
+    item = Item(text=request.POST['text'], list=list_)
     try:
         item.full_clean()
         item.save()
@@ -28,7 +29,7 @@ def view_list(request, list_id):
 
     if request.method == "POST":
         try:
-            item = Item(text=request.POST['item_text'], list=list_)
+            item = Item(text=request.POST['text'], list=list_)
             item.full_clean()
             item.save()
             return redirect(list_)
