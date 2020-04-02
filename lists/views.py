72cde3e090265527c6aad4c3787a797f7d34481e
Ketuael <ketuael@gmail.com>
2020-04-02 13:12:45 +0200

    my_lists.html - zmieniono schemat (zada danych uzytkownika i przypisanych do niego list); Zaczeto prace nad dostosowaniem nizszych warstw do tego.

--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import get_user_model
 from .models import Item, List
 from lists.forms import ExistingListItemForm, ItemForm
+
+User = get_user_model()
 
 # Create your views here.
 
@@ -12,7 +15,9 @@ def home_page(request):
 def new_list(request):
     form = ItemForm(data=request.POST)
     if form.is_valid():
-        list_ = List.objects.create()
+        list_ = List()
+        list_.owner = request.user
+        list_.save()
         form.save(for_list=list_)
         return redirect(list_)
     else:
@@ -31,6 +36,7 @@ def view_list(request, list_id):
 
 
 def my_lists(request, email):
-    return render(request, 'lists/my_lists.html')
+    owner = User.objects.get(email=email)
+    return render(request, 'lists/my_lists.html', {'owner': owner})
 
 
