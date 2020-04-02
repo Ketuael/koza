907523fd2e720726439ef0b64cdd7548a4a49051
Ketuael <ketuael@gmail.com>
2020-04-02 19:28:20 +0200

    Stworzono test dla dzielenia list, lekki refractoring funkcji w zestawie testowym

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -1,9 +1,13 @@
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from django.contrib.auth import BACKEND_SESSION_KEY, SESSION_KEY, get_user_model
+from django.conf import settings
+from django.contrib.sessions.backends.db import SessionStore
 from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
 import time
 
+User = get_user_model()
 MAX_WAIT = 2
 
 
@@ -14,6 +18,20 @@ class FunctionalTest(StaticLiveServerTestCase):
 
     def tearDown(self):
         self.browser.quit()
+
+    def create_pre_authenticated_session(self, email):
+        user = User.objects.create(email=email)
+        session = SessionStore()
+        session[SESSION_KEY] = user.pk
+        session[BACKEND_SESSION_KEY] = settings.AUTHENTICATION_BACKENDS[0]
+        session.save()
+        ## to set a cookie we need to first visit the domain. 404 pages load the quickest!
+        self.browser.get(self.live_server_url + "/404_no_such_url/")
+        self.browser.add_cookie(dict(
+            name=settings.SESSION_COOKIE_NAME,
+            value=session.session_key,
+            path='/',
+        ))
 
     def wait(fn):
         def modified_fn(*args, **kwargs):
