9114ec686dd5841d1b04ec6d4c5982d6389d2f16
Ketuael <ketuael@gmail.com>
2020-04-04 13:47:15 +0200

    Dzielenie listy - stworzono form w list.html; dodano porawny url; stworzono podstawowy view

--- functional_tests/test_sharing.py
+++ functional_tests/test_sharing.py
@@ -39,8 +39,6 @@ class SharingTest(FunctionalTest):
         # The page updates to say that it's shared with Oniciferous:
         list_page.share_list_with('oniciferous@example.com')
 
-        list_page.share_list_with('oniciferous@example.com')
-
         # Oniciferous now goes to the lists page with his browser
         self.browser = oni_browser
         MyListsPage(self).go_to_my_lists_page()
--- lists/views.py
+++ lists/views.py
@@ -31,6 +31,11 @@ def view_list(request, list_id):
     return render(request, 'lists/list.html', {'list': list_, "form": form})
 
 
+def share_list(request, list_id):
+    list_ = List.objects.get(id=list_id)
+    return redirect(list_)
+
+
 def my_lists(request, email):
     owner = User.objects.get(email=email)
     return render(request, 'lists/my_lists.html', {'owner': owner})
