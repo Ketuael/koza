11ee3c46a666605a5ce69fa3de78469b87137cfe
Ketuael <ketuael@gmail.com>
2020-04-04 14:32:28 +0200

    Dzielenie listy - zmieniono model (shared_with - ManyToManyField); not sure, czy poprawnie

--- lists/models.py
+++ lists/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 from django.urls import reverse
 from django.conf import settings
+from accounts.models import User
 
 # Create your models here.
 
 
 class List(models.Model):
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, blank=True, null=True, on_delete=models.CASCADE)
+    shared_with = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="shared_lists")
 
     def get_absolute_url(self):
         return reverse('view_list', args=[self.id])
@@ -20,6 +22,9 @@ class List(models.Model):
     @property
     def name(self):
         return self.item_set.first().text
+
+    def __add__(self, shared_with):
+        shared_with.add()
 
 
 class Item(models.Model):
